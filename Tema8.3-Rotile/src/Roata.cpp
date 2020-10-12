7fbc2a1b6ff4fcbadb5ce571b948ad1c939246ed
pitutnelea <55551834+pitutnelea@users.noreply.github.com>
2020-10-12 23:39:26 +0300

    Update Roata.cpp

--- Tema8.3-Rotile/src/Roata.cpp
+++ Tema8.3-Rotile/src/Roata.cpp
@@ -19,6 +19,8 @@ bool Roata::IsFlat()
     if (_anvelopa.GetPresiune() < 1) {return true;}
     //else if (_anvelopa.GetPresiune() >1) {return false;}
 }
+//în metoda Roata::IsFlat() nu mai trebuia să testezi ceva pe ramura de else. 
+//Dat fiind că tu ai testat ai lăsat pe dinafară cazul în care presiunea anvelopei e chiar 1.
 
 void Roata::SchimbaRoata(Anvelopa anvelopa, Janta janta)
 {
